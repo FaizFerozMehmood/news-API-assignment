d334470ccc78b5a5ccc95d491333a931b613742c
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-07-11 11:39:06 +0500

    js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const fetchData = async()=>{
         const response = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2024-06-10&sortBy=publishedAt&apiKey=78eeb81244cd4d79a1ab6bf2ed4d089f
 `)
         const jsondata = await response.json();
-        console.log(jsondata.articles
-        );
-        // for(let i = 0; i <jsondata.articles.articles;i++){
-        //     console.log("hhh",jsondata.articles.articles);
-        // }
+        // console.log(jsondata.articles
+        // );
+        for(let i = 0; i <jsondata.articles.length;i++){
+            console.log(jsondata.articles[i]);
+        }
             //urlToImage
             //description
 
