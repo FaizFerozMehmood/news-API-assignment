85565986558dbb949aaca6f4cf5477c92bd442c1
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-07-13 01:16:29 +0500

    about to complete

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const container = document.querySelector(".container")
+const userInput = document.getElementById('userInput')
+const get_News = document.getElementById('get_News')
+get_News.addEventListener('click',function(){
+    const output = userInput.value;
+    console.log(output);
+    userInput.innerText="";
+
 const fetchData = async()=>{
     try{
-        const response = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2024-06-10&sortBy=publishedAt&apiKey=78eeb81244cd4d79a1ab6bf2ed4d089f
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${output}&from=2024-06-12&sortBy=publishedAt&apiKey=78eeb81244cd4d79a1ab6bf2ed4d089f
+
 `)
         const jsondata = await response.json();
         // console.log(jsondata.articles
         // );
         for(let i = 0; i <jsondata.articles.length;i++){
             console.log(jsondata.articles[i]);
+            container.innerHTML+=`
+            <div class="card" style="width: 18rem;">
+  <img src=${jsondata.articles[i].urlToImage} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">${jsondata.articles[i].title}.</p>
+    <a href=${jsondata.articles[i].url} class="btn btn-primary">See more...</a>
+  </div>
+</div>
+            `
         }
-            //urlToImage
+            //
             //description
 
 
@@ -20,3 +38,4 @@ const fetchData = async()=>{
    
 }
 fetchData()
+})
